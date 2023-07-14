bad5c516f1e03aa533c34bdb2e21c640089a45ed
ScottMastro <scott-mastro@hotmail.com>
2023-07-14 17:03:16 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E4 Discourse Component - randomly roating banner
+Places a banner below the header. Will randomly be replaced with different banners as user refreshes.
---
+++ plugin.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# name: discourse-e4-banner
+# version: 1.0.0
+# authors: ScottMastro
+# url: https://github.com/ScottMastro/discourse-e4-banner-plugin
+# required_version: 2.7.0
+# transpile_js: true
+
+enabled_site_setting :enable_banner
+
+
